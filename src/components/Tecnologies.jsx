bd278d2c1aec75cab3f40fa37e141588dc60a0e0
Steven Victoria <102372427+TevenV27@users.noreply.github.com>
2023-08-16 03:57:32 -0500

    Update Tecnologies.jsx

--- src/components/Tecnologies.jsx
+++ src/components/Tecnologies.jsx
@@ -55,7 +55,7 @@ export default function Tecnologies() {
               <div className='image-box' key={`duplicate-${index}`}>
                 <img
                   className='image-tech'
-                  src={`shttps://raw.githubusercontent.com/TevenV27/server-images/main/projects/technologies/${image}`}
+                  src={`https://raw.githubusercontent.com/TevenV27/server-images/main/projects/technologies/${image}`}
                   alt='technology'
                 />
               </div>
